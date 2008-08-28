0c4fa83630bdf034ef98feb040d39f8df8de8cbe
Michael Hale <mikehale@gmail.com>
2008-08-28 12:05:14 -0400

    move thin tasks to seperate file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ set :default_stage, "production"
 require 'capistrano/ext/multistage'
 require 'spacesuit/recipes/multistage_patch'
 require 'spacesuit/recipes/common'
+require 'config/recipes/thin'
 
 set :application, "bloat"
 set :rails_env, "production"
@@ -16,20 +17,3 @@ set :repository,  "git@github.com:kohlhofer/rubyurl.git"
 set :deploy_via, :remote_cache
 set :keep_releases, 30
 set :git_enable_submodules, 1
-
-namespace :thin do
-  %w{start stop restart}.each {|action|
-    desc "#{action} thin"
-    task action.to_sym do
-      run "sudo thin #{action} --config /etc/thin/#{application}.yml"
-    end
-  }
-end
-
-namespace :deploy do
-  %w(start stop restart).each do |action|
-    task action.to_sym do 
-      thin.send action 
-    end
-  end
-end
