00ede6097b7ed6d1a8722a90c736b2cf1d247f5a
Michael Hale <mikehale@gmail.com>
2008-08-31 04:38:02 -0400

    use console not dbconsole

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,10 +9,10 @@ role :db,  domain, :primary => true
 
 
 namespace :console do
-  desc "connect to remote rails dbconsole"
-  task :db do
+  desc "connect to remote rails console"
+  task :default do
     input = ''
-    run "cd #{current_path} && script/dbconsole RAILS_ENV=#{rails_env}" do |channel, stream, data|
+    run "cd #{current_path} && script/console #{rails_env}" do |channel, stream, data|
       next if data.chomp == input.chomp || data.chomp == ''
       print data
       channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
