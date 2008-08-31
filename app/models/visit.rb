df2539a08404dd8ae6403019264278c3380e1ca1
Michael Hale <mikehale@gmail.com>
2008-08-31 01:22:58 -0400

    using new rails

--- app/models/visit.rb
+++
@@ -1,3 +0,0 @@
-class Visit < ActiveRecord::Base
-  belongs_to :link
-end
--- db/migrate/003_add_index_on_website_url.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexOnWebsiteUrl < ActiveRecord::Migration
-  def self.up
-    execute "CREATE INDEX website_url_index ON links (website_url(200));"
-  end
-
-  def self.down
-    execute "DROP INDEX website_url_index on links;"
-  end
-end
