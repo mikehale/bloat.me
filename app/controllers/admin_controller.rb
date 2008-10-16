71f54ed34118dbc31707faa56e859f137a813ae2
Michael Hale <mikehale@gmail.com>
2008-10-16 16:42:36 -0400

    show last 10 links

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,17 @@ class AdminController < ApplicationController
   before_filter :authorize, :except => [:login]
   skip_before_filter :verify_authenticity_token
   
+  def index
+    @links = Link.find(:all, :limit => 10, :order => "updated_at DESC")
+  end
+  
   def logout
     session[:password] = nil
   end
     
   private
   def authorize
-    session[:password] = params[:password]
+    session[:password] = params[:password] if params[:password]
     unless session[:password] == ADMIN_PASS
       redirect_to :action => :login
     end
