b071c7f4549d6368d5e215035cf5bf9439749ec9
Michael Hale <mikehale@gmail.com>
2009-05-21 10:54:19 -0400

    disable admin controller for now

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,22 @@
 class AdminController < ApplicationController
-  before_filter :authorize, :except => [:login]
-  
-  def index
-    @links = Link.find(:all, :limit => 10, :order => "updated_at DESC")
-  end
-  
-  def logout
-    session[:password] = nil
-    redirect_to :action => :index
-  end
-    
-  private
-  def authorize
-    session[:password] = params[:password] if params[:password]
-    unless session[:password] == ADMIN_PASS
-      redirect_to :action => :login
-    end
-    true
-  end
+  # before_filter :authorize, :except => [:login]
+  # 
+  # def index
+  #   @links = Link.find(:all, :limit => 10, :order => "updated_at DESC")
+  # end
+  # 
+  # def logout
+  #   session[:password] = nil
+  #   redirect_to :action => :index
+  # end
+  #   
+  # private
+  # def authorize
+  #   session[:password] = params[:password] if params[:password]
+  #   unless session[:password] == ADMIN_PASS
+  #     redirect_to :action => :login
+  #   end
+  #   true
+  # end
 
 end
